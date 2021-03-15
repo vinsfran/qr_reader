d71250211130a354f0c1849235909596e401bde6
Vicente Insfran <vicente.insfran@asuncion.gov.py>
2021-03-15 10:26:20 -0300

    148. Diseño de la aplicación

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:qr_reader/widgets/custom_navigationbar_widget.dart';
+import 'package:qr_reader/widgets/scan_button_widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('Historial'),
+        centerTitle: true,
+        actions: [
+          IconButton(icon: Icon(Icons.delete_forever), onPressed: () {})
+        ],
+      ),
       body: Center(child: Text('Home page')),
+      bottomNavigationBar: CustomNavigationbarWidget(),
+      floatingActionButton: ScanButtonWidget(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
