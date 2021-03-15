99460e60dc98364d13f180fe941cfa905722a022
Vicente Insfran <vicente.insfran@asuncion.gov.py>
2021-03-15 11:32:50 -0300

    151. Provider como gestor de estado

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import 'package:qr_reader/pages/home_page.dart';
 import 'package:qr_reader/pages/mapa_page.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'QR Reader',
-      initialRoute: 'home',
-      routes: {
-        'home': (BuildContext context) => HomePage(),
-        'mapa': (BuildContext context) => MapaPage()
-      },
-      theme: ThemeData(
-        primaryColor: Colors.deepPurple,
-        floatingActionButtonTheme:
-            FloatingActionButtonThemeData(backgroundColor: Colors.deepPurple),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+            create: (BuildContext context) => new UiProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'QR Reader',
+        initialRoute: 'home',
+        routes: {
+          'home': (BuildContext context) => HomePage(),
+          'mapa': (BuildContext context) => MapaPage()
+        },
+        theme: ThemeData(
+          primaryColor: Colors.deepPurple,
+          floatingActionButtonTheme:
+              FloatingActionButtonThemeData(backgroundColor: Colors.deepPurple),
+        ),
       ),
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:qr_reader/widgets/custom_navigationbar_widget.dart';
 import 'package:qr_reader/widgets/scan_button_widget.dart';
 import 'package:qr_reader/pages/mapas_page.dart';
 import 'package:qr_reader/pages/direcciones_page.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -28,8 +30,11 @@ class HomePage extends StatelessWidget {
 class _HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Obtener el selected menu opt
+    final uiProvider = Provider.of<UiProvider>(context);
+
     // Cambiar para mostrar la pagina respectiva
-    final currentIndex = 0;
+    final currentIndex = uiProvider.selectedMenuOpt;
     switch (currentIndex) {
       case 0:
         return MapasPage();
