3f1fae89586673db095cbbaa01c5a7476e76e87d
Vicente Insfran <vicente.insfran@asuncion.gov.py>
2021-03-15 13:52:04 -0300

    154. Leer un código QR

--- lib/widgets/scan_button_widget.dart
+++ lib/widgets/scan_button_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButtonWidget extends StatelessWidget {
   @override
@@ -6,7 +7,11 @@ class ScanButtonWidget extends StatelessWidget {
     return FloatingActionButton(
       elevation: 0,
       child: Icon(Icons.filter_center_focus),
-      onPressed: () {},
+      onPressed: () async {
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+            '#3D8BEF', 'Cancelar', false, ScanMode.QR);
+        print(barcodeScanRes);
+      },
     );
   }
 }
