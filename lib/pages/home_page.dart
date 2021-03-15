be51e88c871c2229eda52e163938d2c26a24a4aa
Vicente Insfran <vicente.insfran@asuncion.gov.py>
2021-03-15 10:48:11 -0300

    150. Cambiar la página dependiendo del tab seleccionado

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+
 import 'package:qr_reader/widgets/custom_navigationbar_widget.dart';
 import 'package:qr_reader/widgets/scan_button_widget.dart';
+import 'package:qr_reader/pages/mapas_page.dart';
+import 'package:qr_reader/pages/direcciones_page.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -14,10 +17,26 @@ class HomePage extends StatelessWidget {
           IconButton(icon: Icon(Icons.delete_forever), onPressed: () {})
         ],
       ),
-      body: Center(child: Text('Home page')),
+      body: _HomePageBody(),
       bottomNavigationBar: CustomNavigationbarWidget(),
       floatingActionButton: ScanButtonWidget(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+}
+
+class _HomePageBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Cambiar para mostrar la pagina respectiva
+    final currentIndex = 0;
+    switch (currentIndex) {
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
+      default:
+        return MapasPage();
+    }
   }
 }
