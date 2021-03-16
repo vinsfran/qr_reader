d33231940ac88556ab859dd2a43f291a3109c7b2
Vicente Insfran <vicente.insfran@asuncion.gov.py>
2021-03-16 14:40:03 -0300

    163. ScanProvider - Centralizar la información de los Scans

---
+++ lib/providers/scan_list_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:qr_reader/providers/db_provider.dart';
+
+class ScanListProvider extends ChangeNotifier {
+  List<ScanModel> scans = [];
+  String tipoSeleccionado = 'http';
+
+  nuevoScan(String valor) async {
+    final nuevoScan = new ScanModel(valor: valor);
+    final id = await DbProvider.db.nuevoScan(nuevoScan);
+    // Asignar el ID de la base de datos al modelo
+    nuevoScan.id = id;
+    if (this.tipoSeleccionado == nuevoScan.tipo) {
+      this.scans.add(nuevoScan);
+      notifyListeners();
+    }
+  }
+}
